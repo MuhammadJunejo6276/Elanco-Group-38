a1e591b62a16fe71bb9eb87d78635dcad212e833
georgp23 <159341797+georgp23@users.noreply.github.com>
2025-03-12 13:21:31 +0000

    Create header.php

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="headerstyle.css">
+    <link rel="icon" href="favicon.png" type="image/x-icon">
+</head>
+<header>
+    <div class="logo">
+        <a href="home.php"><img src="logo.png" alt="Logo"></a>
+    </div>
+</header>
