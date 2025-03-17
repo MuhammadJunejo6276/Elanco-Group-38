15ada4cc59e02e08301f2160f5150dfe4f976e48
Muhammad Junejo <161955638+MBuxJunejo@users.noreply.github.com>
2025-03-17 10:18:52 +0000

    Update calorie_burn.php

--- calorie_burn.php
+++ calorie_burn.php
@@ -36,9 +36,13 @@
     </nav>
 
     <div class="footer">
-        <img src="ElancoPics/fblogo.png" alt="Facebook">
-        <img src="ElancoPics/instalogo.png" alt="Instagram">
-        <img src="ElancoPics/twitterlogo.png" alt="Twitter">
+        <div class="social-icons">
+            <img src="ElancoPics/emailicon.webp" alt="Email">
+            <img src="ElancoPics/fblogo.png" alt="Facebook">
+            <img src="ElancoPics/instalogo.png" alt="Instagram">
+            <img src="ElancoPics/twitterlogo.png" alt="Twitter">
+        </div>
+        <div class="rights">&copy; 2025 Elanco. All rights reserved.</div>
     </div>
 
     <script>
@@ -56,6 +60,5 @@
             }
         });
     </script>
-
 </body>
 </html>
