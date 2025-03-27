4b902d6d6667e5c45893e6c1e19f6ae5d73c1423
MuhammadJunejo6276 <c4026744@hallam.shu.ac.uk>
2025-03-27 14:29:35 +0000

    Update weight.php

--- weight.php
+++ weight.php
@@ -90,8 +90,24 @@ try {
                         datasets: [{
                             label: 'Average Weight',
                             data: <?php echo json_encode($weights); ?>,
-                            backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                            borderColor: 'rgba(75, 192, 192, 1)',
+                            backgroundColor: [
+                                'rgba(75, 192, 192, 0.8)',
+                                'rgba(54, 162, 235, 0.8)',
+                                'rgba(255, 206, 86, 0.8)',
+                                'rgba(153, 102, 255, 0.8)',
+                                'rgba(255, 159, 64, 0.8)',
+                                'rgba(255, 99, 132, 0.8)',
+                                'rgba(201, 203, 207, 0.8)'
+                            ],
+                            borderColor: [
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)',
+                                'rgba(255, 99, 132, 1)',
+                                'rgba(201, 203, 207, 1)'
+                            ],
                             borderWidth: 1,
                         }]
                     },
