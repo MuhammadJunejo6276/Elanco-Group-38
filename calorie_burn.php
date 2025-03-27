08593576d12250dc161d7fa0f4cd9593957c242e
MuhammadJunejo6276 <c4026744@hallam.shu.ac.uk>
2025-03-27 16:02:20 +0000

    Update calorie_burn.php

--- calorie_burn.php
+++ calorie_burn.php
@@ -143,8 +143,24 @@ $current_page = basename($_SERVER['PHP_SELF']);
                         datasets: [{
                             label: 'Calories Burned Per Day',
                             data: <?php echo json_encode($calories) ?>,
-                            backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                            borderColor: 'rgba(75, 192, 192, 1)',
+                            backgroundColor: [
+                                'rgba(75, 192, 192, 0.8)',
+                                'rgba(54, 162, 235, 0.8)',
+                                'rgba(255, 206, 86, 0.8)',
+                                'rgba(153, 102, 255, 0.8)',
+                                'rgba(255, 159, 64, 0.8)',
+                                'rgba(255, 99, 132, 0.8)',
+                                'rgba(201, 203, 207, 0.8)'
+                            ],
+                            borderColor: [
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)',
+                                'rgba(255, 99, 132, 1)',
+                                'rgba(201, 203, 207, 1)'
+                            ],
                             borderWidth: 2,
                             pointRadius: 5,
                             pointHoverRadius: 7
