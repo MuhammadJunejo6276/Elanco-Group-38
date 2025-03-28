76c8d87312d48110db1772cbc9553f04d7540cc1
MuhammadJunejo6276 <c4026744@hallam.shu.ac.uk>
2025-03-28 11:58:35 +0000

    Update login.php

--- login.php
+++ login.php
@@ -134,15 +134,7 @@ try {
         </form>
     </div>
 
-    <div class="footer">
-        <div class="social-icons">
-            <img src="ElancoPics/emailicon.webp" alt="Email">
-            <img src="ElancoPics/fblogo.png" alt="Facebook">
-            <img src="ElancoPics/instalogo.png" alt="Instagram">
-            <img src="ElancoPics/twitterlogo.png" alt="Twitter">
-        </div>
-        <div class="rights">&copy; 2025 Elanco. All rights reserved.</div>
-    </div>
+    <?php include 'footer.php'?>
 
 </body>
 </html>
