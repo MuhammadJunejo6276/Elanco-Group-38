7197f84df1902750d24c51d918baf5531ec3857d
georgp23 <159341797+georgp23@users.noreply.github.com>
2025-03-28 00:14:37 +0000

    Linked footer.php

--- current_activity.php
+++ current_activity.php
@@ -77,15 +77,7 @@ try {
     <div>
         <H2>Current Activity</H2>
     </div>
-    <div class="footer">
-        <div class="social-icons">
-            <img src="ElancoPics/emailicon.webp" alt="Email">
-            <img src="ElancoPics/fblogo.png" alt="Facebook">
-            <img src="ElancoPics/instalogo.png" alt="Instagram">
-            <img src="ElancoPics/twitterlogo.png" alt="Twitter">
-        </div>
-        <div class="rights">&copy; 2025 Elanco. All rights reserved.</div>
-    </div>
+<?php include 'footer.php'?>
 
     <script>
         function toggleDropdown() {
