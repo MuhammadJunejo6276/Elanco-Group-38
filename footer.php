72f8ed1d2064073dfdea6ce43d75290f0e53eb10
georgp23 <159341797+georgp23@users.noreply.github.com>
2025-03-28 00:11:34 +0000

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,9 @@
+<div class="footer">
+        <div class="social-icons">
+            <img src="ElancoPics/emailicon.webp" alt="Email">
+            <img src="ElancoPics/fblogo.png" alt="Facebook">
+            <img src="ElancoPics/instalogo.png" alt="Instagram">
+            <img src="ElancoPics/twitterlogo.png" alt="Twitter">
+        </div>
+        <div class="rights">&copy; 2025 Elanco. All rights reserved.</div>
+ </div>
