dae40ecf040c06205be111581322746f90e083bf
MuhammadJunejo6276 <c4026744@hallam.shu.ac.uk>
2025-03-31 01:33:23 +0100

    Time scale added

--- barking_frequency.php
+++ barking_frequency.php
@@ -61,7 +61,6 @@ try {
             die("Invalid week selected");
         }
     } else {
-        // Default to latest week
         $dates = [];
         for ($i = 6; $i >= 0; $i--) {
             $date = clone $maxDate;
